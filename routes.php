31edf0db0c1c7a183daa2adcb42e10ead4d15379
Christopher Owens <cowensdev@gmail.com>
2024-06-05 19:32:15 -0400

    Add logout

--- routes.php
+++ routes.php
@@ -5,6 +5,7 @@ $router->get('/auth/login', 'UserController@login');
 $router->get('/auth/register', 'UserController@create');
 
 $router->post('/auth/register', 'UserController@store');
+$router->post('/auth/logout', 'UserController@logout');
 $router->post('/auth/login', 'UserController@authenticate');
 
 // API
