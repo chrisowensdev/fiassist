cc0fe9f47d0769d7a4f71c11ac85a85cabd7d4fb
Christopher Owens <cowensdev@gmail.com>
2024-07-04 09:12:59 -0400

    Uncomment errors from the login page

--- App/views/users/forgotPassword.view.php
+++ App/views/users/forgotPassword.view.php
@@ -4,9 +4,9 @@
 <div class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-500 mx-6">
         <h2 class="text-4xl text-center font-bold mb-4">Reset Password</h2>
-        <!-- <?= loadPartial('errors', [
-                    'errors' => $errors ?? []
-                ]) ?> -->
+        <?= loadPartial('errors', [
+            'errors' => $errors ?? []
+        ]) ?>
         <p class="mb-3 text-center">
             Enter your email address and you will be send an email with a link to reset your password.
         </p>
